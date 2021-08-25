131f7bf25a5d7f9d020f463579849817a23fe0c8
jk <jk@DESKTOP-6BISE36>
2021-08-25 09:44:59 +0530

    added the billing process UI

--- src/WebMVC/AdminDashboard/Controllers/BillingController.cs
+++ src/WebMVC/AdminDashboard/Controllers/BillingController.cs
@@ -29,5 +29,51 @@ namespace AdminDashboard.Controllers
         {
             return View();
         }
+        public IActionResult billingmonth()
+        {
+            billingIndexViewModel model = new billingIndexViewModel(); 
+            model.yearList = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "--Select Year--"},
+                new SelectListItem {Text = "2020", Value = "1"},
+                new SelectListItem {Text = "2021", Value = "2"},
+                new SelectListItem {Text = "2022", Value = "3"},
+                new SelectListItem {Text = "2023", Value = "4"},
+                new SelectListItem {Text = "2024", Value = "5"},
+                new SelectListItem {Text = "2025", Value = "6"},
+                new SelectListItem {Text = "2026", Value = "7"},
+                new SelectListItem {Text = "2027", Value = "8"},
+                new SelectListItem {Text = "2028", Value = "9"},
+                new SelectListItem {Text = "2029", Value = "10"},
+                new SelectListItem {Text = "2030", Value = "11"}
+
+            };
+            model.monthList = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "--Select Month--"},
+                new SelectListItem {Text = "January", Value = "2"},
+                new SelectListItem {Text = "February", Value = "3"},
+                new SelectListItem {Text = "March", Value = "4"},
+                new SelectListItem {Text = "April", Value = "5"},
+                new SelectListItem {Text = "May", Value = "6"},
+                new SelectListItem {Text = "June", Value = "7"},
+                new SelectListItem {Text = "July", Value = "8"},
+                new SelectListItem {Text = "August", Value = "9"},
+                new SelectListItem {Text = "September", Value = "10"},
+                new SelectListItem {Text = "October", Value = "11"},
+                new SelectListItem {Text = "November", Value = "12"},
+                new SelectListItem {Text = "December", Value = "13"}
+            };
+            return View(model);
+        }
+        public IActionResult BillingRate()
+        {
+            billingRate model = new billingRate();
+            return View(model);
+        }
+        public IActionResult InvoicePreview()
+        {
+            return View();
+        }
     }
 }
