3d0590a6d59681236b29f56c11113a11193757cc
vijaylaxmi4k04@gmail.com <vijaylaxmi4k04@gmail.com>
2021-09-16 17:52:50 +0530

    ui changes is done

--- src/WebMVC/AdminDashboard/ViewModels/UserManagement.cs
+++ src/WebMVC/AdminDashboard/ViewModels/UserManagement.cs
@@ -14,9 +14,11 @@ namespace InvoiceMicroServices.WebMVC.AdminDashboard.ViewModels
         public List<SelectListItem> employeeList { get; set; }
 
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required.")]
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please enter a valid User Id")]
         public string email { get; set; }
         [Display(Name = "Mobile")]
+        [RegularExpression(@"\d{10}", ErrorMessage = "Please enter 10 digit Mobile No.")]
         [Required(ErrorMessage = "Mobile Number is required.")]
         public string mobile { get; set; }
         [Display(Name = "First Name")]
